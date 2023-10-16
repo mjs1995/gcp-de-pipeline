3d94b22fa01b3dd6d7b5dd272e67d114f9755b3a
mjs <munmun2004@naver.com>
2023-10-17 00:20:30 +0900

    ✨ feat: add airflow helm

---
+++ terraform-airflow-gke/airflow.tf
@@ -0,0 +1,37 @@
+provider "kubernetes" {
+  host             = "https://${google_container_cluster.primary.endpoint}"
+  token            = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(
+    google_container_cluster.primary.master_auth[0].cluster_ca_certificate
+  )
+}
+
+resource "kubernetes_namespace" "airflow" {
+  depends_on = [google_container_cluster.primary]
+  metadata {
+    name = "airflow"
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${google_container_cluster.primary.endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(
+      google_container_cluster.primary.master_auth[0].cluster_ca_certificate
+    )
+  }
+}
+
+resource "helm_release" "airflow" {
+  name       = "airflow"
+  repository = "https://airflow-helm.github.io/charts"
+  chart      = "airflow"
+  namespace  = kubernetes_namespace.airflow.metadata[0].name
+
+  set {
+    name  = "webserverSecretKeySecretName"
+    value = "my-webserver-secret"
+  }
+
+}
--- terraform-airflow-gke/main.tf
+++ terraform-airflow-gke/main.tf
@@ -1,3 +1,5 @@
+data "google_client_config" "default" {}
+
 provider "google" {
   credentials = file(var.credentials_path)
   project     = var.project_id
@@ -31,43 +33,4 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
       "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
-}
-
-provider "kubernetes" {
-  load_config_file = false
-  host             = google_container_cluster.primary.endpoint
-  token            = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(
-    google_container_cluster.primary.master_auth[0].cluster_ca_certificate
-  )
-}
-
-resource "kubernetes_namespace" "airflow" {
-  metadata {
-    name = "airflow"
-  }
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = google_container_cluster.primary.endpoint
-    token                  = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(
-      google_container_cluster.primary.master_auth[0].cluster_ca_certificate
-    )
-    namespace              = kubernetes_namespace.airflow.metadata[0].name
-  }
-}
-
-resource "helm_release" "airflow" {
-  name       = "airflow"
-  repository = "https://airflow-helm.github.io/charts"
-  chart      = "airflow"
-  namespace  = kubernetes_namespace.airflow.metadata[0].name
-
-  set {
-    name  = "webserverSecretKeySecretName"
-    value = "my-webserver-secret"
-  }
-
 }
