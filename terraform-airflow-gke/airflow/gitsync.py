6a84859b4f9efe6349c6bf3c573cb07ef9a86c23
mjs <munmun2004@naver.com>
2023-10-20 07:56:36 +0900

    🚨 fix: resolve conflict

--- terraform-airflow-gke/airflow/gitsync.py
+++ terraform-airflow-gke/airflow/gitsync.py
@@ -2,31 +2,29 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
+
 def print_hello():
     print("Hello, Airflow!")
 
+
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
 }
 
 dag = DAG(
-    'hello_airflow',
-    description='Simple tutorial DAG',
+    "hello_airflow",
+    description="Simple tutorial DAG",
     schedule_interval=timedelta(days=1),
     start_date=datetime(2023, 10, 17),
     catchup=False,
-    default_args=default_args
+    default_args=default_args,
 )
 
-t1 = PythonOperator(
-    task_id='print_hello',
-    python_callable=print_hello,
-    dag=dag
-)
+t1 = PythonOperator(task_id="print_hello", python_callable=print_hello, dag=dag)
 
 t1
