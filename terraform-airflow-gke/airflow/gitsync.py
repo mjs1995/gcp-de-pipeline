9f8b0546dee9b23892a63ddf9bfe69842bb7c808
mjs <munmun2004@naver.com>
2023-11-11 21:10:22 +0900

    feat: add minio dag

--- terraform-airflow-gke/airflow/gitsync.py
+++ terraform-airflow-gke/airflow/gitsync.py
@@ -1,26 +1,42 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
-from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator
-from airflow.utils.dates import days_ago
-
-from datetime import datetime, timedelta
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
 import requests
-import logging
-import json
+import pandas as pd
+import boto3
+from io import BytesIO
 
-dag = DAG(
-    dag_id="slack_test",
-    start_date=days_ago(1),
-    max_active_runs=1,
-    catchup=False,
-    schedule_interval="@once",
-)
 
-send_slack_message = SlackWebhookOperator(
-    task_id="send_slack",
-    slack_webhook_conn_id="slack_webhook",
-    message="Hello slack",
-    dag=dag,
-)
+def download_and_upload_to_minio():
+    url = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-01.parquet"
+    response = requests.get(url)
+    data = response.content
+    df = pd.read_parquet(BytesIO(data))
+    csv_data = df.to_csv(index=False).encode("utf-8")
+    s3_client = boto3.client(
+        "s3",
+        endpoint_url="http://127.0.0.1:9000",
+        aws_access_key_id="minioadmin",
+        aws_secret_access_key="minioadmin",
+    )
 
-send_slack_message
+    s3_client.put_object(
+        Bucket="airflow-minio", Key="nyc_data.csv", Body=BytesIO(csv_data)
+    )
+
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2023, 1, 1),
+    "retries": 1,
+}
+
+with DAG(
+    "download_and_upload_to_minio", default_args=default_args, schedule_interval=None
+) as dag:
+
+    upload_task = PythonOperator(
+        task_id="download_upload_nyc_data", python_callable=download_and_upload_to_minio
+    )
+
+upload_task
